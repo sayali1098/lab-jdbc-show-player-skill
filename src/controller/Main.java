67f1cb940ae05e4635c5753b1ab85ece238756a8
sayali1098 <sauamritkar@gmail.com>
2020-07-15 17:01:15 +0530

    FSD0620B102

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,33 @@
+package controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main{
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+		
+		SkillDAO skilldao = new SkillDAO();
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter skill id to get skill details");
+		long id= Long.parseLong(br.readLine());
+		Skill skill = null;
+		skill.setSkillId(id);
+		
+		ArrayList<Skill> list  = (ArrayList<Skill>) skilldao.listAllSkills();
+		System.out.println("List of all Skills");
+		
+		for(Skill s: list) {
+			System.out.println(s.getSkillId() + "\t" + s.getSkillName());
+		}
+		
+		
+	}
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,31 @@
+package dao;
 
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO {
+	
+	List<Skill> list = new ArrayList<Skill>();
+	public List< Skill > listAllSkills () throws ClassNotFoundException, SQLException, IOException {
+		
+		ConnectionManager con = new ConnectionManager();
+		Statement stmt=  con.getConnection().createStatement();
+		ResultSet rs =stmt.executeQuery("SELECT name FROM skill ORDER BY name ASC");
+		
+		while(rs.next())
+		{
+			long skillId = rs.getLong(1);
+			String skillName = rs.getString(2);
+			
+			Skill skill = new Skill(skillId, skillName);
+			list.add(skill);
+		}
+		return list;
+	}	
+}
